f751e7d1eeec5683cebfb1f6ae3c6a04f0776f47
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-03 02:13:42 +0600

    implemented course publish action

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,29 @@
-const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+  });
+  return (
+    <div className="flex flex-col space-y-2">
+      {courses.map((course) => (
+        <Link href={`/teacher/courses/${course.id}`} key={course.id}>
+          {course.title}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default CoursesPage;
