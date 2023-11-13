90dc6b451cee3f226872d3dc50d36a71837943b5
masud-rana44 <mdmasudrana4488@gmail.com>
2023-11-14 00:08:13 +0600

    fix: logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-      className="cursor-pointer"
-    />
+    <div className="space-y-1">
+      <Image
+        height={50}
+        width={50}
+        alt="logo"
+        src="/logo.svg"
+        className="cursor-pointer"
+      />
+      <h1 className="font-bold text-xl text-[#2880b0]">Knowledge War</h1>
+    </div>
   );
 };
