5da7fa4423306e7de9b5661cf611b5737f99c5f4
masud-rana44 <mdmasudrana4488@gmail.com>
2023-11-14 00:41:43 +0600

    temp file added

---
+++ components/temp.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const temp = () => {
+  return (
+    <div>
+      temp
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, unde.
+        Voluptatum facilis quibusdam veritatis sapiente exercitationem,
+        blanditiis consectetur aliquam, explicabo mollitia saepe, possimus
+        tempore libero minus et doloribus? Quo dicta expedita, id adipisci
+        cupiditate doloribus ex quibusdam placeat deleniti consequatur
+        aspernatur ea vitae, deserunt blanditiis alias qui earum fugiat tempora
+        aliquam dolore tenetur accusamus dolorum, nemo delectus. Molestias
+        incidunt tempora consectetur eum nulla, aut molestiae laboriosam nam
+        possimus reiciendis facilis iusto libero repellat saepe veritatis sed
+        natus architecto, numquam, fugit suscipit? Nisi voluptatem in expedita
+        deserunt quis reiciendis officiis quaerat. Quisquam, incidunt
+        recusandae. Perspiciatis debitis non facere qui itaque eos voluptatem ut
+        quod totam, dicta atque accusantium modi nostrum fuga nam ad quidem
+        provident iusto deleniti doloremque ducimus. Modi expedita ipsam sunt
+        facilis voluptate alias vel obcaecati non, nostrum commodi molestiae
+        impedit voluptas quia earum omnis assumenda velit quis. Voluptatum nisi
+        officia maxime architecto quia nam similique blanditiis cumque,
+        assumenda eos itaque vel ipsa ipsum quos repudiandae, adipisci amet
+        nostrum ad? Ullam cum quia est adipisci? Esse sequi ex perspiciatis
+        dolores voluptatem neque minus rerum omnis. Corrupti facilis facere
+        laudantium minus error nam eos labore, ipsam accusantium. Harum quae,
+        incidunt inventore debitis consequuntur quos odio velit saepe enim nulla
+        nisi magnam animi, quasi totam facere voluptatibus dicta doloremque
+        voluptate? Cumque nesciunt, dolor et optio perspiciatis doloribus quia
+        sit quasi ea impedit qui incidunt tempora maiores assumenda perferendis,
+        minus suscipit sunt rem vel, explicabo nobis animi. Nulla praesentium
+        placeat vitae? Ea veritatis, architecto labore tenetur ratione hic
+        voluptates deleniti magnam obcaecati aperiam quidem harum. Minus
+        suscipit nesciunt consectetur cumque, odit quo, adipisci eveniet
+        obcaecati incidunt quaerat mollitia rem fugit repellat totam
+        perspiciatis maiores, quidem unde saepe magnam recusandae delectus! Sint
+        pariatur ex animi fugiat nam omnis delectus itaque, veniam
+        necessitatibus iure laboriosam dolore odio deserunt quas, eius
+        laudantium! Aperiam corporis sint ab blanditiis laborum! Illo sequi
+        reprehenderit libero quos quasi facilis iusto eligendi, amet quam labore
+        repudiandae ad, adipisci itaque. Aperiam doloremque libero a tempore
+        voluptatem sit eum, neque atque amet. Provident dignissimos
+        exercitationem libero earum, perferendis voluptatum. Quisquam molestias
+        hic vero consectetur odio? Aut consequatur cumque sequi nobis nemo vero
+        obcaecati, fugit doloribus quas aliquid quos in deleniti quae hic.
+        Laborum distinctio earum sit at, porro facilis quasi possimus eligendi
+        laudantium quae iste architecto aliquam odio fuga ducimus quod obcaecati
+        similique, magnam asperiores, tempore hic impedit. Nostrum illo enim
+        iure vero aut iste officia labore cumque soluta minus voluptas similique
+        voluptate facere doloribus quos incidunt delectus, nesciunt, dolor omnis
+        accusantium? Amet totam maiores, voluptatibus cum maxime fugit. Sit
+        autem magnam expedita facere sapiente unde, fugit cupiditate omnis harum
+        non totam, vitae provident exercitationem rem sequi facilis adipisci
+        saepe reprehenderit minima atque, culpa consequatur in? Libero animi
+        numquam expedita corrupti temporibus obcaecati veniam, rem ut quo neque
+        iste sit atque earum ipsam sequi ad repudiandae perferendis architecto
+        magnam quasi. Omnis ab nam vitae ducimus, voluptatibus, molestiae culpa
+        sed reiciendis laborum vero architecto magni eos cupiditate nesciunt
+        veritatis nemo atque consequuntur? Vel et, a praesentium reprehenderit
+        doloremque laboriosam odio corrupti reiciendis minima!
+      </p>
+    </div>
+  );
+};
+
+export default temp;
